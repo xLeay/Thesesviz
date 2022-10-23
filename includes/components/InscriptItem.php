ea0a94fbf799e955a2d360f80e5650738535d513
xLeay <LarkyRiver@gmail.com>
2022-10-23 22:26:13 +0200

    Script Import terminé mais trop long. à optimiser

--- includes/components/InscriptItem.php
+++ includes/components/InscriptItem.php
@@ -7,7 +7,7 @@
 
 <form action="../pages/auth/cnx.php?from=signup" method="post" class="connect_form">
     <label for="username">Login</label>
-    <input class="input logi" type="text" name="username" placeholder="Nom d'utilisateur" required>
+    <input class="input logi" type="text" name="username" placeholder="Nom d'utilisateur" required autofocus>
     <label for="password">Mot de passe</label>
     <input class="input passw" type="password" name="password" placeholder="Mot de passe" required>
     <button class="btn auth_btn" type="submit" value="Inscription">Inscription</button>
--- includes/functions/script_import.php
+++ includes/functions/script_import.php
@@ -2,337 +2,159 @@
 
 include '/Laragon/www/Thesesviz/includes/auth/conf.php';
 
-// $json = file_get_contents('../extract_theses.json');
-// $data = json_decode($json, true);
-
-
-
-// [
-//     {
-//     "these_sur_travaux": "non",
-//     "date_soutenance": "2006-01-01",
-//     "directeurs_these": [
-//       {
-//         "nom": "Meynadier",
-//         "prenom": "Laure",
-//         "idref": "114430489"
-//       }
-//     ],
-//     "etablissements_soutenance": [
-//       {
-//         "idref": "027542084",
-//         "nom": "Paris 7"
-//       }
-//     ],
-//     "discipline": {
-//       "fr": "Géochimie"
-//     },
-//     "oai_set_specs": [
-//       "ddc:550",
-//       "ddc:540"
-//     ],
-//     "president_jury": {},
-//     "@version": "1",
-//     "iddoc": 712269,
-//     "nnt": "2006PA077132",
-//     "ecoles_doctorales": [],
-//     "embargo": null,
-//     "status": "soutenue",
-//     "source": "sudoc",
-//     "accessible": "non",
-//     "langue": "fr",
-//     "@timestamp": "2022-03-01T13:04:48.489Z",
-//     "code_etab": "PA07",
-//     "cas": "cas5",
-//     "titres": {
-//       "en": "Neodymium isotopic stratigraphy in the Indian Ocean : Oceanic paleocirculation and Continental erosion",
-//       "fr": "Stratigraphie isotopique de néodyme dans l'Océan Indien : Paléocirulation océanique et Erosion continentale"
-//     },
-//     "resumes": {
-//       "fr": "La composition isotopique du Néodyme dans les sédiments marins est un outil potentiel pour étudier la circulation océanique dans le passé."
-//     },
-//     "sujets": {},
-//     "membres_jury": [],
-//     "partenaires_recherche": [],
-//     "sujets_rameau": [
-//       {
-//         "type_vedette": "vedetteRameauGenreForme",
-//         "element_entree": {
-//           "contenu": null,
-//           "idref": null
-//         },
-//         "subdivisions": []
-//       },
-//       {
-//         "type_vedette": "vedetteRameauNomCommun",
-//         "element_entree": {
-//           "contenu": "Géochimie",
-//           "idref": "027282694"
-//         },
-//         "subdivisions": []
-//       },
-//       {
-//         "type_vedette": "vedetteRameauNomCommun",
-//         "element_entree": {
-//           "contenu": "Érosion",
-//           "idref": "027388514"
-//         },
-//         "subdivisions": []
-//       },
-//       {
-//         "type_vedette": "vedetteRameauNomCommun",
-//         "element_entree": {
-//           "contenu": "Néodyme",
-//           "idref": "031779107"
-//         },
-//         "subdivisions": [
-//           {
-//             "type": "subdivisionGeographique",
-//             "contenu": "Indien, Océan",
-//             "idref": "027405478"
-//           }
-//         ]
-//       },
-//       {
-//         "type_vedette": "vedetteRameauNomCommun",
-//         "element_entree": {
-//           "contenu": "Tectonique des plaques",
-//           "idref": "027253619"
-//         },
-//         "subdivisions": []
-//       },
-//       {
-//         "type_vedette": "vedetteRameauNomCommun",
-//         "element_entree": {
-//           "contenu": "Stratigraphie",
-//           "idref": "027249565"
-//         },
-//         "subdivisions": []
-//       }
-//     ],
-//     "rapporteurs": [],
-//     "auteurs": [
-//       {
-//         "nom": "Gourlan",
-//         "prenom": "Alexandra",
-//         "idref": "118063383"
-//       }
-//     ]
-//   }
-// ]
-
-
-// foreach ($data as $product) {
-//     $stmt = $conn->prepare('insert into product(name, price, quantity) values(:name, :price, :quantity)');
-//     $stmt->bindValue('name', $product->name);
-//     $stmt->bindValue('price', $product->price);
-//     $stmt->bindValue('quantity', $product->quantity);
-//     $stmt->execute();
-// }
-
 
 if (isset($_POST['buttomImport'])) {
 
     $json = file_get_contents('/Laragon/www/Thesesviz/includes/extract_theses.json');
     $data = json_decode($json, true);
-
-    // // ETABLISSEMENT
-    // $idref_etablissement = $data['etablissements_soutenance']['idref'];
-    // $nom_etablissement = $data['etablissements_soutenance']['nom'];
-
-    // // SOUTENIR
-    // $date_soutenance = $data['date_soutenance'];
-
-    // // THESE
-    // $accessible = $data['accessible'];
-    // $embargo = $data['embargo'];
-    // $nnt = $data['nnt'];
-    // $oai_set_specs = $data['oai_set_specs'];
-    // $resume = $data['resumes'];
-    // $soutenue = $data['status'];
-    // $sur_travaux = $data['these_sur_travaux'];
-    // $titre = $data['titres'];
-    // $discipline = $data['discipline'];
-
-    // // REPOSER
-
-    // // SUJET
-    // $libellé_sujet = $data['sujets'];
-
-    // // ASSISTER
-    // // on doit trouver le role : directeur de these, president de jury, membre de jury, rapporteur, auteur de la these à partir de l'idThese et de l'idPersonne
-    // $role = $data['role'];
-
-    // // PERSONNE
-    // $directeurs_theseNom = $data['directeurs_these']['nom'];
-    // $directeurs_thesePrenom = $data['directeurs_these']['prenom'];
-    // $directeurs_theseIdref = $data['directeurs_these']['idref'];
-
-    // $president_juryNom = $data['president_jury']['nom'];
-    // $president_juryPrenom = $data['president_jury']['prenom'];
-    // $president_juryIdref = $data['president_jury']['idref'];
-
-    // $membres_juryNom = $data['membres_jury']['nom'];
-    // $membres_juryPrenom = $data['membres_jury']['prenom'];
-    // $membres_juryIdref = $data['membres_jury']['idref'];
-
-    // $rapporteursNom = $data['rapporteurs']['nom'];
-    // $rapporteursPrenom = $data['rapporteurs']['prenom'];
-    // $rapporteursIdref = $data['rapporteurs']['idref'];
-
-    // $auteursNom = $data['auteurs']['nom'];
-    // $auteursPrenom = $data['auteurs']['prenom'];
-    // $auteursIdref = $data['auteurs']['idref'];
-
-
-
     $i = 0;
 
     foreach ($data as $these) {
 
-        $personnes_noms = [];
-        $personnes_prenoms = [];
-        $personnes_idref = [];
+        $time_start = microtime(true);
+        // INSERTION NUMERO UNE (THESE)
 
-        $directeurs_these = $these['directeurs_these'];
-        foreach ($directeurs_these as $directeur_these) {
-            $directeurs_theseNom = $directeur_these['nom'];
-            $directeurs_thesePrenom = $directeur_these['prenom'];
-            $directeurs_theseIdref = $directeur_these['idref'];
-
-            array_push($personnes_noms, $directeurs_theseNom);
-            array_push($personnes_prenoms, $directeurs_thesePrenom);
-            array_push($personnes_idref, $directeurs_theseIdref);
-        }
-
-        
-        $president_jury = $these['president_jury'];
-        if ($president_jury != null) {
-            $president_juryNom = $president_jury['nom'];
-            $president_juryPrenom = $president_jury['prenom'];
-            $president_juryIdref = $president_jury['idref'];
-
-            array_push($personnes_noms, $president_juryNom);
-            array_push($personnes_prenoms, $president_juryPrenom);
-            array_push($personnes_idref, $president_juryIdref);
-        }
-
-        // $president_jury != null ? debug('-> DIRECTEUR ->', $directeurs_these, '-> PRESIDENT ->', $president_jury) : debug('-> DIRECTEUR ->', $directeurs_these);
-        
-        
-        
-        $membres_jury = $these['membres_jury'];
-        foreach ($membres_jury as $membre) {
-            $membres_juryNom = $membre['nom'];
-            $membres_juryPrenom = $membre['prenom'];
-            $membres_juryIdref = $membre['idref'];
-            
-            array_push($personnes_noms, $membres_juryNom);
-            array_push($personnes_prenoms, $membres_juryPrenom);
-            array_push($personnes_idref, $membres_juryIdref);
-        }
-        
-        
-        
-        $rapporteurs = $these['rapporteurs'];
-        foreach ($rapporteurs as $rapporteur) {
-            $rapporteursNom = $rapporteur['nom'];
-            $rapporteursPrenom = $rapporteur['prenom'];
-            $rapporteursIdref = $rapporteur['idref'];
-
-            array_push($personnes_noms, $rapporteursNom);
-            array_push($personnes_prenoms, $rapporteursPrenom);
-            array_push($personnes_idref, $rapporteursIdref);
-        }
-        
-        $auteurs = $these['auteurs'];
-        foreach ($auteurs as $auteur) {
-            $auteursNom = $auteur['nom'];
-            $auteursPrenom = $auteur['prenom'];
-            $auteursIdref = $auteur['idref'];
-
-            array_push($personnes_noms, $auteursNom);
-            array_push($personnes_prenoms, $auteursPrenom);
-            array_push($personnes_idref, $auteursIdref);
-        }
-        
-        // debug($personnes_noms, $personnes_prenoms, $personnes_idref);
-
-        
-        // debug('------------------------------------------------------------------    ' . $i, '-> PERSONNES_NOMS ->', $personnes_noms, '-> PERSONNES_PRENOMS ->', $personnes_prenoms, '-> PERSONNES_IDREF ->', $personnes_idref);
-        debug('------------------------------------------------------------------    ' . $i, '-> PERSONNES_NOMS ->', $personnes_noms);
-        $i++;
-
-        // $sql = "INSERT INTO personne (nom, prenom, idref) VALUES (:nom, :prenom, :idref)";
-        // $insertion = $conn->prepare($sql);
-        // // $insertion->bindParam(':nom', $directeurs_theseNom);
-        // // $insertion->bindParam(':prenom', $directeurs_thesePrenom);
-        // // $insertion->bindParam(':idref', $directeurs_theseIdref);
-        // foreach ($personnes_noms as $personne_nom) {
-        //     $insertion->bindParam(':nom', $personne_nom);
-        // }
-        // foreach ($personnes_prenoms as $personne_prenom) {
-        //     $insertion->bindParam(':prenom', $personne_prenom);
-        // }
-        // foreach ($personnes_idref as $personne_idref) {
-        //     $insertion->bindParam(':idref', $personne_idref);
-        // }
+        $accessible = ($these['accessible'] == "non") ? 0 : 1;
+        $embargo = $these['embargo'];
+        $nnt = $these['nnt'];
+        $oai_set_specs = $these['oai_set_specs'];
+        $resume = $these['resumes'];
+        isset($these['resumes']['fr']) ? $resume = $these['resumes']['fr'] : $resume = NULL;
+        $soutenue = ($these['status'] == "non") ? 0 : 1;
+        $sur_travaux = ($these['these_sur_travaux'] == "non") ? 0 : 1;
+        $titre = end($these['titres']);
+        $discipline = $these['discipline']['fr'];
 
 
-        $sql = "INSERT INTO personne (nom) VALUES (:nom)";
+        $sql = "INSERT INTO these (these_accessible, embargo, nnt, oai_set_specs, resume, soutenue, sur_travaux, titre, discipline) VALUES (:these_accessible, :embargo, :nnt, :oai_set_specs, :resume, :soutenue, :sur_travaux, :titre, :discipline)";
         $insertion = $conn->prepare($sql);
-        foreach ($personnes_noms as $personne_nom) {
-            $insertion->bindParam(':nom', $personne_nom);
+        $insertion->bindParam(':these_accessible', $accessible);
+        $insertion->bindParam(':embargo', $embargo);
+        $insertion->bindParam(':nnt', $nnt);
+        foreach ($oai_set_specs as $oai_set_spec) {
+            $insertion->bindParam(':oai_set_specs', $oai_set_spec);
+        }
+        $insertion->bindParam(':resume', $resume);
+        $insertion->bindParam(':soutenue', $soutenue);
+        $insertion->bindParam(':sur_travaux', $sur_travaux);
+        $insertion->bindParam(':titre', $titre);
+        $insertion->bindParam(':discipline', $discipline);
+
+        $insertion->execute();
+
+        $time_end = microtime(true);
+        $time = $time_end - $time_start;
+
+        echo "$time secondes\n";
+
+        
+        // INSERTION NUMERO DEUX (ETABLISSEMENT)
+
+        $etablissements = $these['etablissements_soutenance'];
+        foreach ($etablissements as $etablissement) {
+            $idref_etablissement = $etablissement['idref'];
+            $nom_etablissement = $etablissement['nom'];
+        }
+
+        $sql2 = "INSERT INTO etablissement (idref, nom) VALUES (:idref, :nom)";
+        $insertion2 = $conn->prepare($sql2);
+        $insertion2->bindParam(':idref', $idref_etablissement);
+        $insertion2->bindParam(':nom', $nom_etablissement);
+
+        $insertion2->execute();
+
+
+        // INSERTION NUMERO TROIS (SOUTENIR)
+
+        $date_soutenance = $these['date_soutenance'];
+
+        $sql3 = "INSERT INTO soutenir (idEtablissement, idThese, date_soutenance) SELECT e.idEtablissement, t.idThese, :date_soutenance FROM `etablissement` e JOIN `these` t ORDER BY t.idThese DESC, e.idEtablissement DESC LIMIT 1";
+
+        $insertion3 = $conn->prepare($sql3);
+        $insertion3->bindParam(':date_soutenance', $date_soutenance);
+
+        $insertion3->execute();
+
+
+        // INSERTION NUMERO QUATRE (SUJETS)
+
+        $libelle_sujet = $these['sujets'];
+        isset($these['sujets']['fr']) ? $libelle_sujet = $these['sujets']['fr'] : $libelle_sujet = NULL;
+
+        $sql4 = "INSERT INTO `sujet` (libelle) SELECT :libelle_sujet WHERE NOT EXISTS (SELECT * FROM `sujet` WHERE libelle = :libelle_sujet)";
+        $insertion4 = $conn->prepare($sql4);
+        if ($libelle_sujet != NULL) {
+            foreach ($libelle_sujet as $libelle) {
+                $insertion4->bindParam(':libelle_sujet', $libelle);
+                $insertion4->execute();
+
+            }
+        }
+        else {
+            $insertion4->bindParam(':libelle_sujet', $libelle_sujet);
+            $insertion4->execute();
         }
 
 
-        try {
-            $insertion->execute();
-            $_POST = array();
-        } catch (PDOException $e) {
-            echo $e->getMessage();
+        // INSERTION NUMERO CINQ (REPOSER)
+
+        $sql5 = "INSERT INTO `reposer` (idSujet, idThese) SELECT s.idSujet, t.idThese FROM `sujet` s JOIN `these` t ORDER BY t.idThese DESC, s.idSujet DESC LIMIT 1";
+        $insertion5 = $conn->prepare($sql5);
+        $insertion5->execute();
+
+
+        // INSERTION NUMERO SIX (PERSONNE) ET NUMERO SEPT (ASSISTER)
+
+        $personnes = array();
+        $personnes[] = $these['directeurs_these'];
+        $personnes[] = $these['president_jury'];
+        $personnes[] = $these['membres_jury'];
+        $personnes[] = $these['rapporteurs'];
+        $personnes[] = $these['auteurs'];
+
+        $sql6 = "INSERT INTO personne (nom, prenom, idref) VALUES (:nom, :prenom, :idref) ON DUPLICATE KEY UPDATE nom = nom, prenom = prenom, idref = idref";
+        $insertion6 = $conn->prepare($sql6);
+
+        $sql7 = "INSERT INTO assister (idPersonne, idThese, role) SELECT p.idPersonne, t.idThese, :role FROM `personne` p JOIN `these` t ORDER BY t.idThese DESC, p.idPersonne DESC LIMIT 1 ON DUPLICATE KEY UPDATE idPersonne = p.idPersonne, idThese = t.idThese, role = :role";
+
+        $insertion7 = $conn->prepare($sql7);
+
+        $role = array();
+        $role[] = 'directeur de these';
+        $role[] = 'president de jury';
+        $role[] = 'membre de jury';
+        $role[] = 'rapporteur';
+        $role[] = 'auteur de la these';
+
+        $roleN = 0;
+
+        foreach ($personnes as $personne) {
+            foreach ($personne as $key => $value) {
+
+                if (gettype($key) == 'string') {
+                    $insertion6->bindParam(':' . $key, $value);
+                } else if (gettype($key) == 'integer') {
+                    $insertion6->bindParam(':nom', $value['nom']);
+                    $insertion6->bindParam(':prenom', $value['prenom']);
+                    $insertion6->bindParam(':idref', $value['idref']);
+                }
+
+                $insertion6->execute();
+            }
+
+            if ($personne != NULL) {
+
+                $insertion7->bindParam(':role', $role[$roleN]);
+                
+                $insertion7->execute();
+            }
+            $roleN++;
         }
+
+        $i++;
+        if ($i == 1) {
+            break;
+        }
+        // break;
     }
 }
-
-// LA THESE
-
-// $accessible = ($these['accessible'] == "non") ? 0 : 1;
-// $embargo = $these['embargo'];
-// $nnt = $these['nnt'];
-// $oai_set_specs = $these['oai_set_specs'];
-// $resume = $these['resumes'];
-// isset($these['resumes']['fr']) ? $resume = $these['resumes']['fr'] : $resume = NULL;
-// $soutenue = ($these['status'] == "non") ? 0 : 1;
-// $sur_travaux = ($these['these_sur_travaux'] == "non") ? 0 : 1;
-// $titre = end($these['titres']);
-// $discipline = $these['discipline']['fr'];
-
-
-// $sql = "INSERT INTO these (these_accessible, embargo, nnt, oai_set_specs, resume, soutenue, sur_travaux, titre, discipline) VALUES (:these_accessible, :embargo, :nnt, :oai_set_specs, :resume, :soutenue, :sur_travaux, :titre, :discipline)";
-// $insertion = $conn->prepare($sql);
-// $insertion->bindParam(':these_accessible', $accessible);
-// $insertion->bindParam(':embargo', $embargo);
-// $insertion->bindParam(':nnt', $nnt);
-// foreach ($oai_set_specs as $oai_set_spec) {
-//     $insertion->bindParam(':oai_set_specs', $oai_set_spec);
-// }
-// $insertion->bindParam(':resume', $resume);
-// $insertion->bindParam(':soutenue', $soutenue);
-// $insertion->bindParam(':sur_travaux', $sur_travaux);
-// $insertion->bindParam(':titre', $titre);
-// $insertion->bindParam(':discipline', $discipline);
-
-
-// L'ÉTABLISSEMENT
-
-// $etablissements = $these['etablissements_soutenance'];
-// foreach ($etablissements as $etablissement) {
-//     $idref_etablissement = $etablissement['idref'];
-//     $nom_etablissement = $etablissement['nom'];
-// }
-
-// $sql = "INSERT INTO etablissement (idref, nom) VALUES (:idref, :nom)";
-// $insertion = $conn->prepare($sql);
-// $insertion->bindParam(':idref', $idref_etablissement);
-// $insertion->bindParam(':nom', $nom_etablissement);
